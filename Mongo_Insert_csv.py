bb7ea87aaf7ae6fdc8a0b0d3b2922ef895bb31f2
Yana Mosiichuk <mosiychukyana@gmail.com>
2019-01-31 12:45:30 +0200

    fix

--- Mongo_Insert_csv.py
+++ Mongo_Insert_csv.py
@@ -28,6 +28,3 @@ if __name__ == "__main__":
 
 print('insert done')
 
-
-# how to run docker
-# docker-compose -f docker-compose.1.yml -f docker-compose.2.yml  -f docker-compose.cnf.yml -f docker-compose.shard.yml up
